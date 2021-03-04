83d4487e7cdac3c74fa46c311bff082ef9aba890
Joshuafc <joshuafc@foxmail.com>
2021-03-04 17:04:30 +0800

    add ice-coon

--- network/ice-conn.go
+++ network/ice-conn.go
@@ -20,8 +20,21 @@ import (
 type IceAgent struct {
 	agent       *ice.Agent
 	conn        *ice.Conn
-	remoteUfrag string
-	remotePwd   string
+	remoteSDP	*IceSDP
+}
+
+type IceSDP struct {
+	frag       string
+	pwd        string
+	candidates []*ice.Candidate
+}
+
+func (i *IceSDP) FromString(str string) error  {
+	return nil
+}
+
+func (i *IceSDP) ToString() (string, error)  {
+	return "", nil
 }
 
 func NewIceAgent(stunAddr string, turnAddr string, turnUser string, turnPwd string) (*IceAgent, error) {
@@ -48,20 +61,45 @@ func (i *IceAgent) Accept() (*ice.Conn, error) {
 	if i.agent == nil {
 		return &ice.Conn{}, nil
 	}
-	return i.agent.Accept(context.TODO(), i.remoteUfrag, i.remotePwd)
+	return i.agent.Accept(context.TODO(), i.remoteSDP.frag, i.remoteSDP.pwd)
 }
 
 func (i *IceAgent) Dial() (*ice.Conn, error) {
 	if i.agent == nil {
 		return &ice.Conn{}, nil
 	}
-	return i.agent.Dial(context.TODO(), i.remoteUfrag, i.remotePwd)
+	return i.agent.Dial(context.TODO(), i.remoteSDP.frag, i.remoteSDP.pwd)
 }
 
 func (i *IceAgent) GetLocalSDP() (string, error) {
-	return "", nil
+	var localSDP IceSDP
+	localSDP.frag, localSDP.pwd = i.agent.GetLocalUserCredentials()
+	candidateChan := make(chan *ice.Candidate)
+	i.agent.OnCandidate(func(c ice.Candidate) {
+		if c == nil{
+			close(candidateChan)
+		} else {
+			candidateChan <- &c
+		}
+	})
+	i.agent.GatherCandidates()
+	for {
+		candidate, ok := <- candidateChan
+		if !ok {
+			break
+		}
+		i.remoteSDP.candidates = append(i.remoteSDP.candidates, candidate)
+	}
+	return localSDP.ToString()
 }
 
 func (i *IceAgent) SetRemoteSDP(sdp string) error {
+	err := i.remoteSDP.FromString(sdp)
+	if err != nil {
+		panic(err)
+	}
+	//i.agent.AddRemoteCandidate()
+
+
 	return nil
 }
